5b05a94ac19c1133195670e6d4ddb49b544d81b2
vixowownmr-wq <vixowow.nmr@gmail.com>
2026-05-15 15:55:22 -0400

    Exercise Rock,Paper n Scissors finished, review this code in the futuregit add . to improve !

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,21 +1,71 @@
-console.log("Hello world!")
-
-
 function getComputerChoice () {
     const numero = Math.random();
 
     switch (true) {
         case numero < 1 / 3:
-            return 'Rock';
+            return "Rock";
         case numero < 2 / 3:
-            return 'Paper';
+            return "Paper";
         default:
-            return 'Scissors';
-    
-            
+            return "Scissors";
     }
 
 }
+const computerChoice = getComputerChoice();
 
-console.log(getComputerChoice());
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+    if(humanChoice === computerChoice) {
+        return "¡¡¡It's a draw!!!"}
+        if(
+            (humanChoice === "Rock" && computerChoice === "Scissors") ||
+            (humanChoice === "Scissors" && computerChoice === "Paper") ||
+            (humanChoice === "Paper" && computerChoice === "Rock")
+        ) {
+            humanScore++;
+            return "You win :)"
+        }
+    else {
+        computerScore++;
+        return "You lose :("
+    }      
+}
+
+
+
+function playGame(i) {
+    function getHumanChoice() {
+        const yourChoice = prompt("Choose yout option, Rock, Paper or Scissors: ");
+        return yourChoice;
+    }
+    
+    
+    for(let i=0;i<5;i++) {
+        const human = getHumanChoice();
+        const computer = getComputerChoice();
+        const result = playRound(human, computer);
+        console.log(result);
+        console.log("TU: " + humanScore + " Computadora: " + computerScore);
+    }
+  
+}
+
+console.log(playGame(5));
+
+
+
+
+
+
+
+
+
+
+
+
 
